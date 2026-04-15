e34f18d0018e2e03fbd5d66be32b53045e9c7118
LouzadaCaetano <csrlouzada@gmail.com>
2026-04-15 19:50:39 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from 'styled-components/native'
+import { theme } from './src/styles/theme'
+import { Routes } from './src/routes'
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  )
+}
